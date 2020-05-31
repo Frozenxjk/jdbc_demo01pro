ebd6f7e9357a813adc992ad7e781a16a0b76e603
FrozenII <frozenii@163.com>
2020-05-31 20:13:48 +0800

    第一版

---
+++ src/main/java/cn/frozen1/test/MybatisTest.java
@@ -0,0 +1,30 @@
+package cn.frozen1.test;
+
+import cn.frozen1.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MybatisTest {
+    public static void main(String[] args) throws IOException {
+        // 指定全局配置文件
+        String resource = "mybatis-config.xml";
+        // 读取配置文件
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        // 构建sqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        // 获取sqlSession
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            // 操作CRUD，第一个参数：指定statement，规则：命名空间+“.”+statementId
+            // 第二个参数：指定传入sql的参数：这里是用户id
+            User user = sqlSession.selectOne("MyMapper.selectUser", 2);
+            System.out.println(user);
+        }
+    }
+}
+
+
